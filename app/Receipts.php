cc2867f9c340811c1e9ba3d592dca87992a1c350
Aashish Agarwal <aasish.agarwal.75@gmail.com>
2016-10-21 03:53:57 +0000

    Created Recipts table & migration

---
+++ app/Receipts.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Receipts extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'receipts';    //
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['bill_number','bill_date','b17_debit','description','invoice_no','invoice_date','procurement_certificate','procurement_date','unit_weight','unit_quantity','value','duty','transport_registration','receipt_timestamp','balance_quantity','balance_value'];
+
+}
+
---
+++ database/migrations/2016_10_20_170428_create_receipts_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReceiptsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('receipts', function (Blueprint $table) {
+		$table->increments('id');
+		$table->string('bill_number')->unique();
+		$table->string('bill_date');
+		$table->string('b17_debit');
+		$table->string('description');
+		$table->string('invoice_no')->nullable();
+		$table->string('invoice_date')->nullable();
+		$table->string('procurement_certificate')->nullable();
+		$table->string('procurement_date')->nullable();
+		$table->string('unit_weight')->nullable();
+		$table->string('unit_quantity')->nullable();
+		$table->string('value');
+		$table->string('duty');
+		$table->string('transport_registration')->nullable();
+		$table->string('receipt_timestamp')->nullable();
+		$table->string('balance_quantity')->nullable();
+		$table->string('balance_value')->nullable();
+		$table->softDeletes();
+		$table->timestamps();
+	});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('receipts');
+    }
+}
