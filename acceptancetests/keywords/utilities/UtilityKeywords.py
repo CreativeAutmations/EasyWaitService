3ef1f7fe4592abc52b6d183ad7dd9430798cd771
Ubuntu <ubuntu@ip-172-31-17-33.us-west-2.compute.internal>
2016-10-23 08:16:35 +0000

    Recovred from Syntax Errors

--- acceptancetests/keywords/utilities/UtilityKeywords.py
+++ acceptancetests/keywords/utilities/UtilityKeywords.py
@@ -5,13 +5,14 @@ import json
 class UtilityKeywords(object):
 
     def __init__(self):
+        print "Initialized Keywords"
 
-    def verify_value_in_dictionary(self, ref_dictioary , serach_key, search_value):
-        if serach_key in ref_dictioary.keys():
-                if ( ref_dictioary[serach_key] != search_value ):
-                        raise Exception('Expected: ' + serach_key + ' Found: ' + ref_dictioary[serach_key])
+    def verify_value_in_dictionary(self, ref_dictioary , search_key, search_value):
+        if search_key in ref_dictioary.keys():
+                if ( ref_dictioary[search_key] != search_value ):
+                        raise Exception('Expected: ' + search_value + ' Found: ' + ref_dictioary[search_key])
         else:
-                raise Exception('Key: ' + serach_key +' Not Found')
+                raise Exception('Key: ' + search_key +' Not Found')
 
 
 
