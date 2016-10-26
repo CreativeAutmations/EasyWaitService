03d7a69bf25bd0626b7cf27a9b532e8416336d48
Ubuntu <ubuntu@ip-172-31-17-33.us-west-2.compute.internal>
2016-10-26 05:48:31 +0000

    Supported Basic UI

--- resources/views/customs.php
+++ resources/views/customs.php
@@ -5,7 +5,6 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 			<!-- Bootstrap -->
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-		<link href="bs/css/bootstrap-theme.min.css" rel="stylesheet">
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
