3e6b8fba1a60984219f517b4076a554f2d846714
Aasish Agarwal <aasish.agarwal.75@gmail.com>
2016-10-28 14:55:43 +0530

    Supported basic UI for customs register

--- app/Http/Controllers/ReceiptsController.php
+++ app/Http/Controllers/ReceiptsController.php
@@ -18,8 +18,7 @@ class ReceiptsController extends Controller
 	 *
 	 * @return Response
 	 */
-   public function CreateReceipt() 
-   {
+   public function CreateReceipt()    {
 		$token = JWTAuth::getToken();
 
 		try { 
@@ -57,4 +56,118 @@ class ReceiptsController extends Controller
 		       return Response::json(false, HttpResponse::HTTP_UNAUTHORIZED);
 		}	
 	}
+
+
+   public function UpdateReceipt( $bill_number) {
+
+		$token = JWTAuth::getToken();
+		try { 
+			$user = JWTAuth::toUser($token);
+		} catch (Exception $e) {
+			       return Response::json(false, HttpResponse::HTTP_UNAUTHORIZED);
+		}	
+
+		try { 
+			$user = JWTAuth::toUser($token);
+			$bill_details = Input::except('bill_number');
+
+			$receipts = Receipts::where('bill_number', '=', $bill_number )->get();
+
+			if( $receipts -> first()){
+				$receipts -> first()->update($bill_details);
+				
+					# Create Audit Record
+					$audit_change_log = json_encode($bill_details);
+					$audit_email = $user->email;
+					$category = 'Receipt';
+					$action = 'Updated';
+					
+					$audit_record_data = array ('bill_number' => $bill_number, 
+												'email' => $audit_email, 
+												'action' => $action, 
+												'change_log' => $audit_change_log, 
+												'category' => $category);
+					$audit = Audit::create($audit_record_data);
+
+					
+				return Response::json(['message' => 'Receipt Updated']);
+			}else{
+				return Response::json(['message' => 'Operation Failed: Receipt with Bill Number: ' . $bill_number . ' Not Found']);
+			}
+		} catch (Exception $e) {
+			return Response::json(['message' => 'Failure Updating Receipt Details for bill_number: ' . $bill_number ]);
+		}	
+	}
+	
+   public function GetReceiptByBillNumber ( $bill_number ) {
+		$token = JWTAuth::getToken();
+		try { 
+			$user = JWTAuth::toUser($token);
+		} catch (Exception $e) {
+			       return Response::json(false, HttpResponse::HTTP_UNAUTHORIZED);
+		}	
+
+		try { 
+			$receipts = Receipts::where('bill_number', '=', $bill_number )->get();
+
+			if( $receipts -> first()){
+				return Response::json($receipts -> first());
+			}else{
+				return Response::json(['message' => 'Operation Failed: Receipt with Bill Number: ' . $bill_number . ' Not Found']);
+			}
+		} catch (Exception $e) {
+			return Response::json(['message' => 'Failure Getting Receipt Details for bill_number: ' . $bill_number ]);
+		}	
+	}
+
+   public function SearchReceipts ( ) {
+		$token = JWTAuth::getToken();
+		try { 
+			$user = JWTAuth::toUser($token);
+		} catch (Exception $e) {
+			return Response::json(false, HttpResponse::HTTP_UNAUTHORIZED);
+		}	
+		
+		$filter_conditions = Input::all();
+		$field = array_keys($filter_conditions)[0];
+		$field_value = $filter_conditions[$field]["value"];
+		$operator = $filter_conditions[$field]["operation"];
+
+		try { 
+			$receipts = Receipts::where($field, $operator , $field_value )->get();
+
+			if( $receipts->isEmpty()){
+				return Response::json( false, ['message' => 'No Matching Records Found'] );
+			} else {
+				return Response::json($receipts);
+			}
+		} catch (Exception $e) {
+			return Response::json( false, ['message' => 'System Error', 'exception' => $e->getMessage()] );
+		}	
+	}
+
+   public function GetAuditTrailForABill ( $bill_number ) {
+		$token = JWTAuth::getToken();
+		try { 
+			$user = JWTAuth::toUser($token);
+		} catch (Exception $e) {
+			return Response::json(false, HttpResponse::HTTP_UNAUTHORIZED);
+		}	
+
+		try { 
+			$audit = Audit::where('bill_number', '=', $bill_number )->get();
+
+			if( $audit->isEmpty()){
+				return Response::json(['message' => 'Operation Failed: Audit Trail with Bill Number: ' . $bill_number . " Not Found"]);
+			} else {
+				foreach ( $audit as $audit_record ) {
+					$audit_record['change_log'] = json_decode($audit_record['change_log']);
+				}
+				return Response::json($audit);
+			}
+		} catch (Exception $e) {
+			return Response::json(['message' => 'Failure Getting Audit Trail for bill_number: ' . $bill_number ]);
+		}	
+	}
+	
 }
