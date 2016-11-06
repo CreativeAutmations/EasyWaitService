ece0759a70f9b6a7afb78f09bfc5ef2ad1faad6a
Aasish Agarwal <aasish.agarwal.75@gmail.com>
2016-11-06 21:47:01 +0530

    Added models and database migrations for organization

--- acceptancetests/keywords/import_record/OrganizationManager.py
+++ acceptancetests/keywords/import_record/OrganizationManager.py
@@ -11,9 +11,9 @@ class OrganizationManager(object):
 
 
     def organization_membership_request(self, access_token, org_id):
-		url = self._hostURL +'/api/user/organization'
+        url = self._hostURL +'/api/user/organization'
         json_payload_list = {}
-		json_payload_list['id'] = org_id;
+        json_payload_list['id'] = org_id;
         call_headers = {'Authorization': 'Bearer '+ access_token}
         r = requests.post(url, json=json_payload_list, headers=call_headers)
         r.raise_for_status()
@@ -22,25 +22,25 @@ class OrganizationManager(object):
     def create_organization(self, access_token,org_name,address,tax_registration,tax_commissionar):
         url = self._hostURL +'/api/organizations'
         json_payload_list = {}
-		json_payload_list['org_name'] = org_name;
-		json_payload_list['address'] = address;
-		json_payload_list['tax_registration'] = tax_registration;
-		json_payload_list['tax_commissionar'] = tax_commissionar;
-		
+        json_payload_list['org_name'] = org_name;
+        json_payload_list['address'] = address;
+        json_payload_list['tax_registration'] = tax_registration;
+        json_payload_list['tax_commissionar'] = tax_commissionar;
+
         call_headers = {'Authorization': 'Bearer '+ access_token}
         r = requests.post(url, json=json_payload_list, headers=call_headers)
         r.raise_for_status()
 
-		server_response = r.json()
-		print server_response
-		return server_response["id"]
+        server_response = r.json()
+        print server_response
+        return server_response["id"]
 
 
     def approve_organization_membership(self, access_token,email):
-		url = self._hostURL +'/api/user/organization'
+        url = self._hostURL +'/api/user/organization'
         json_payload_list = {}
-		json_payload_list['email'] = email;
-		json_payload_list['action'] = 'approve';
+        json_payload_list['email'] = email;
+        json_payload_list['action'] = 'approve';
         call_headers = {'Authorization': 'Bearer '+ access_token}
         r = requests.put(url, json=json_payload_list, headers=call_headers)
         r.raise_for_status()
