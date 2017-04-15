822a2709f0dec3b923571b610d138e23082ff6b9
Ubuntu <ubuntu@ip-172-31-17-33.us-west-2.compute.internal>
2017-04-15 10:32:19 +0000

    Created Queue Controller

--- app/Http/Controllers/QueueController.php
+++ app/Http/Controllers/QueueController.php
@@ -4,83 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Organization as Organization;
-use App\UserOrganization as UserOrganization;
-
-use Tymon\JWTAuth\Facades\JWTAuth;
-use Tymon\JWTAuth\Exceptions\TokenExpiredException;
-use Tymon\JWTAuth\Exceptions\TokenInvalidException;
-use Tymon\JWTAuth\Exceptions\JWTException;
-
 use App\Http\Requests;
-use Illuminate\Http\Response as HttpResponse;
-use Illuminate\Support\Facades\Input;
-use Response;
-
 
 class QueueController extends Controller
 {
-    public function checkToken()
-    {
-        try 
-        {
-            if (! $user = JWTAuth::parseToken()->authenticate())
-            {
-                return [
-                    'error' => true,
-                    'code'  => 10,
-                    'details'  => [
-                        'message'   => 'User not found by given token'
-                    ]
-                ];
-            }
-
-        } catch (TokenExpiredException $e) {
-
-            return [
-                'error' => true,
-                'code'  => 11,
-                'details'  => [
-                    'message'   => 'Token Expired'
-                ]
-            ];
-
-        } catch (TokenInvalidException $e) {
-
-            return [
-                'error' => true,
-                'code'  => 12,
-                'details'  => [
-                    'message'   => 'Invalid Token'
-                ]
-            ];
-
-        } catch (JWTException $e) {
-
-            return [
-                'error' => true,
-                'code'  => 13,
-                'details'  => [
-                    'message'   => 'Token absent'
-                ]
-            ];
-
-        }
-
-        return ['error' => false, 'token' => JWTAuth::getToken()];
-    }
-	
-
-   /**
-	 * Create Queue
-	 *
-	 * @return Response
-	 */
-   public function CreateQueue()    {
-		return response('OK', 200)
-			->header('Content-Type', 'application/json')
-			->setContent([
-			'id' => 124,
-			'name' => "Queue 2" ]);
-	}
+    //
 }
